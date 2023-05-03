a0e6e2ed4e5b845279ce1020ee849f0ccf8df0ed
Jacobo <jacoboburbano2004gmail.com>
2023-05-03 14:36:50 -0500

    Aplicamos el menu mobile y un pooco de responsive a estas

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
 const email = document.querySelector(".navbar-email")
 const desktopMenuInteractive = document.querySelector(".desktop-menu")
+const burgerIcon = document.querySelector(".menu")
+const mobileMenu = document.querySelector(".mobile-menu")
 
 
 email.addEventListener('click', toggleMenu)
 
 function toggleMenu() {
     desktopMenuInteractive.classList.toggle("inactive")
+}
+
+burgerIcon.addEventListener("click" , toggleMobileMenu)
+
+function toggleMobileMenu() {
+    mobileMenu.classList.toggle("inactive")
 }
