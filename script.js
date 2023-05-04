9e04e120afab4fac57f5d76009ff6c253ad4b378
Jacobo <jacoboburbano2004gmail.com>
2023-05-04 16:00:20 -0500

    Usamos las propiedades de comprobar valor de etiqueta class o tambien se puede con la propiedad getAttribute()

--- script.js
+++ script.js
@@ -2,16 +2,52 @@ const email = document.querySelector(".navbar-email")
 const desktopMenuInteractive = document.querySelector(".desktop-menu")
 const burgerIcon = document.querySelector(".menu")
 const mobileMenu = document.querySelector(".mobile-menu")
+const carIcon = document.getElementById("icon_Car")
+const asideDetail = document.querySelector(".product-detail")
 
 
 email.addEventListener('click', toggleMenu)
 
 function toggleMenu() {
+    
+    if(desktopMenuInteractive.classList.contains("inactive")){
+        if(!asideDetail.classList.contains("inactive")){
+            asideDetail.classList.add("inactive")
+        }
+            
+    }
+    if(!desktopMenuInteractive.classList.contains("inactive")){
+        if(!asideDetail.classList.contains("inactive")){
+            desktopMenuInteractive.classList.remove("inactive")
+        }
+    }
     desktopMenuInteractive.classList.toggle("inactive")
 }
 
 burgerIcon.addEventListener("click" , toggleMobileMenu)
 
 function toggleMobileMenu() {
+    if(mobileMenu.getAttribute("class") === "mobile-menu inactive"){
+        if (asideDetail.getAttribute("class") === "product-detail") {
+            asideDetail.classList.add("inactive")
+        }
+    }
     mobileMenu.classList.toggle("inactive")
+}
+
+carIcon.addEventListener("click" , toggleProductDetail)
+
+function toggleProductDetail() {
+        if(asideDetail.getAttribute("class") === "product-detail"){
+            if(mobileMenu.getAttribute("class") === "mobile-menu"){
+                mobileMenu.classList.add("inactive")
+            }
+        }
+        asideDetail.classList.toggle("inactive")
+        if(!desktopMenuInteractive.classList.contains("inactive")){
+            if(!asideDetail.classList.contains("inactive"))
+                desktopMenuInteractive.add("inactive")
+            
+        }
+        
 }
