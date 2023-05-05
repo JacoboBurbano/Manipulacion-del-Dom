12ca9a717686474b3f412f164eda970037a4c225
Jacobo <jacoboburbano2004gmail.com>
2023-05-05 15:10:34 -0500

    Haciendo Manipulacion del DOM

--- script.js
+++ script.js
@@ -4,50 +4,46 @@ const burgerIcon = document.querySelector(".menu")
 const mobileMenu = document.querySelector(".mobile-menu")
 const carIcon = document.getElementById("icon_Car")
 const asideDetail = document.querySelector(".product-detail")
-
+const cardsContainer = document.querySelector(".product-card")
+const containerCards = document.querySelector(".cards-container")
 
 email.addEventListener('click', toggleMenu)
 
 function toggleMenu() {
-    
-    if(desktopMenuInteractive.classList.contains("inactive")){
-        if(!asideDetail.classList.contains("inactive")){
-            asideDetail.classList.add("inactive")
-        }
-            
-    }
-    if(!desktopMenuInteractive.classList.contains("inactive")){
-        if(!asideDetail.classList.contains("inactive")){
-            desktopMenuInteractive.classList.remove("inactive")
-        }
-    }
+    mobileMenu.classList.add("inactive")
+    asideDetail.classList.add("inactive")
     desktopMenuInteractive.classList.toggle("inactive")
 }
 
 burgerIcon.addEventListener("click" , toggleMobileMenu)
 
 function toggleMobileMenu() {
-    if(mobileMenu.getAttribute("class") === "mobile-menu inactive"){
-        if (asideDetail.getAttribute("class") === "product-detail") {
-            asideDetail.classList.add("inactive")
-        }
-    }
+    asideDetail.classList.add("inactive")
+    desktopMenuInteractive.classList.add("inactive")
     mobileMenu.classList.toggle("inactive")
 }
 
 carIcon.addEventListener("click" , toggleProductDetail)
 
 function toggleProductDetail() {
-        if(asideDetail.getAttribute("class") === "product-detail"){
-            if(mobileMenu.getAttribute("class") === "mobile-menu"){
-                mobileMenu.classList.add("inactive")
-            }
-        }
-        asideDetail.classList.toggle("inactive")
-        if(!desktopMenuInteractive.classList.contains("inactive")){
-            if(!asideDetail.classList.contains("inactive"))
-                desktopMenuInteractive.add("inactive")
-            
-        }
-        
+    desktopMenuInteractive.classList.add("inactive")
+    mobileMenu.classList.add("inactive")
+    asideDetail.classList.toggle("inactive")
 }
+
+let almacenarProductos = []
+almacenarProductos.push({
+    name : "Bike",
+    price : 120,
+    img : "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+})
+almacenarProductos.push({
+    name : "Pantalla",
+    price : 320,
+    img : "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+})
+almacenarProductos.push({
+    name : "Computador",
+    price : 680,
+    img : "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+})
