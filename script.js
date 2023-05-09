191b9f2d359241395768f7996cb989c6e6804947
Jacobo <jacoboburbano2004gmail.com>
2023-05-09 12:00:10 -0500

    Manipulacion Dinamica productos, faltante del tercer paso

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ const burgerIcon = document.querySelector(".menu")
 const mobileMenu = document.querySelector(".mobile-menu")
 const carIcon = document.getElementById("icon_Car")
 const asideDetail = document.querySelector(".product-detail")
-const cardsContainer = document.querySelector(".product-card")
 const containerCards = document.querySelector(".cards-container")
 
 email.addEventListener('click', toggleMenu)
@@ -31,19 +30,46 @@ function toggleProductDetail() {
     asideDetail.classList.toggle("inactive")
 }
 
-let almacenarProductos = []
-almacenarProductos.push({
+let almacenarElectricosProductos = []
+almacenarElectricosProductos.push({
     name : "Bike",
     price : 120,
     img : "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
 })
-almacenarProductos.push({
+almacenarElectricosProductos.push({
     name : "Pantalla",
     price : 320,
     img : "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
 })
-almacenarProductos.push({
+almacenarElectricosProductos.push({
     name : "Computador",
     price : 680,
     img : "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
 })
+function productosPedidos(almacenarProductos){
+    for(const product of almacenarProductos){
+        const divProductCard= document.createElement("div")
+        divProductCard.classList.add("product-card")
+        const imgProduct = document.createElement("img")
+        imgProduct.setAttribute("src" , product.img)
+        const divInfoProduct = document.createElement("div")
+        divInfoProduct.classList.add("product-info")
+        const divPriceName = document.createElement("div")
+        const productPrice = document.createElement("p")
+        productPrice.innerText = "$" + product.price
+        const productName = document.createElement("p")
+        productName.innerText = product.name
+        divPriceName.appendChild(productPrice)
+        divPriceName.appendChild(productName)
+        const figure = document.createElement("figure")
+        const imgIcon = document.createElement("img")
+        imgIcon.setAttribute("src","./icons/bt_add_to_cart.svg", "alt=''" )
+        figure.appendChild(imgIcon)
+
+        divProductCard.append(imgProduct, divInfoProduct, divPriceName)
+        divInfoProduct.append(divPriceName, figure)
+
+        containerCards.appendChild(divProductCard)
+    }
+}
+productosPedidos(almacenarElectricosProductos)
